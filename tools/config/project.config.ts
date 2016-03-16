96428659b32cc39d74d4128435df86db4e421930
d3viant0ne <joshuaw@easymetrics.com>
2016-03-16 03:14:43 -0500

    Add Material2 modules to project

--- tools/config/project.config.ts
+++ tools/config/project.config.ts
@@ -7,10 +7,23 @@ export class ProjectConfig extends SeedConfig {
 
   constructor() {
     super();
-    // this.APP_TITLE = 'Put name of your app here';
-    let additional_deps: InjectableDependency[] = [
+    this.APP_TITLE = 'Angular2-Seed Materialized';
+    let additional_deps: Array<any> = [
       // {src: 'jquery/dist/jquery.min.js', inject: 'libs'},
       // {src: 'lodash/lodash.min.js', inject: 'libs'},
+      { src: '@angular2-material/core/core.js', inject: 'libs' },
+      { src: '@angular2-material/button/button.js', inject: 'libs' },
+      { src: '@angular2-material/button/button.css', inject: true },
+      { src: '@angular2-material/card/card.js', inject: 'libs' },
+      { src: '@angular2-material/card/card.css', inject: true },
+      { src: '@angular2-material/checkbox/checkbox.js', inject: 'libs' },
+      { src: '@angular2-material/checkbox/checkbox.css', inject: true },
+      { src: '@angular2-material/progress-circle/progress-circle.js', inject: 'libs' },
+      { src: '@angular2-material/progress-circle/progress-circle.css', inject: true },
+      { src: '@angular2-material/sidenav/sidenav.js', inject: 'libs' },
+      { src: '@angular2-material/sidenav/sidenav.css', inject: true },
+      { src: '@angular2-material/toolbar/toolbar.js', inject: 'libs' },
+      { src: '@angular2-material/toolbar/toolbar.css', inject: true }
     ];
 
     const seedDependencies = this.NPM_DEPENDENCIES;
